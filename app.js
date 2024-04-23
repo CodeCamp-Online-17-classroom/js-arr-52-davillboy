4ea48009ff46ebb18d934a08388389a6eb728dbd
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 01:21:57 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+let arr = [3.24, 2.78, 3.86, 1.37, 0.52];
+
+let max = Math.max(...arr);
+let min = Math.min(...arr);
+
+console.log("ค่ามากสุด:", max); 
+console.log("ค่าน้อยสุด:", min); 
